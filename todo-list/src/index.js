5a17601fdc972498e8955b458d2fa4c0e510d724
usman ali <usman.ali@381solutions.com>
2021-10-07 11:42:48 +0500

    app and todo container

---
+++ todo-list/src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
+import TodoContainer from "./components/TodoContainer"
+
+
+
+ReactDOM.render(<TodoContainer />,document.getElementById('root'))
