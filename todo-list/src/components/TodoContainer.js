d246f99d210270563530fa1b6ed36ae409af8ee9
uali <usmanaliua.uali@gmail.com>
2021-10-07 13:06:13 +0500

    T1

--- todo-list/src/components/TodoContainer.js
+++ todo-list/src/components/TodoContainer.js
@@ -1,13 +1,38 @@
 import React from 'react'
-
+import Header from './Header';
+import TodosList from "./TodosList"
 class TodoContainer extends React.Component{
+
+
+
+    state ={
+        todos:[
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+              },
+              {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+              },
+              {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+              }
+        ]
+    }
+
+
     render(){
         return(
-            <div>
-                <h1>Hello from doto app</h1>
-                <p>I am in react component</p>
-            </div>
-        )
+            <React.Fragment>
+                <Header />
+                <TodosList todos={this.state.todos}/>
+           </React.Fragment>
+        );
     }
 }
 
